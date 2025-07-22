923387755e27d3aeabc9020a351f0f6b9d5c5bcd
Amar Biradar <176784906+ambir513@users.noreply.github.com>
2025-07-22 09:50:50 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     type: "website",
     url: "https://vbenterprises.work/",
     images: [
-      "https://photos.app.goo.gl/fPyAy83vMz994swM7",
+      "https://user-gen-media-assets.s3.amazonaws.com/gpt4o_images/915593da-0e3b-4890-9d86-6f048f0a0fb6.png",
     ],
   },
 };
